634342d8fde0c1779b6d62ccaebd350f9318ef70
Zeph LaFassett <zeph@lafassett.com>
2013-07-28 15:08:49 -0700

    added better window sizes, added space entities, renderer packages

--- jastroblast-core/src/org/doublelong/jastroblast/JastroBlast.java
+++ jastroblast-core/src/org/doublelong/jastroblast/JastroBlast.java
@@ -3,24 +3,26 @@ package org.doublelong.jastroblast;
 import org.doublelong.jastroblast.screen.JastroScreen;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
 
 public class JastroBlast extends Game
 {
 	public final String WINDOW_TITLE = "jAstroBlast";
-	public final int WINDOW_WIDTH = 600;
 	public final int WINDOW_HEIGHT = 600;
+	public final int WINDOW_WIDTH = WINDOW_HEIGHT / 9 * 10;
 
 	private JastroScreen jastroScreen;
 
 	@Override
 	public void create()
 	{
+		Texture.setEnforcePotImages(false);
 		this.initialize();
 		this.setScreen(this.jastroScreen);
 	}
 
 	private void initialize()
 	{
-		this.jastroScreen = new JastroScreen();
+		this.jastroScreen = new JastroScreen(this);
 	}
 }
--- jastroblast-core/src/org/doublelong/jastroblast/screen/JastroScreen.java
+++ jastroblast-core/src/org/doublelong/jastroblast/screen/JastroScreen.java
@@ -1,15 +1,44 @@
 package org.doublelong.jastroblast.screen;
 
+import org.doublelong.jastroblast.JastroBlast;
+import org.doublelong.jastroblast.entity.Space;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class JastroScreen implements Screen
 {
+	private final OrthographicCamera cam;
+	private final SpriteBatch batch;
+	private final Space space;
+
+	public JastroScreen(JastroBlast game)
+	{
+		this.space = new Space(game);
+		this.batch = new SpriteBatch();
+		this.cam = new OrthographicCamera(Space.WIDTH, Space.HEIGHT);
+		this.cam.setToOrtho(false, Space.WIDTH, Space.HEIGHT);
+
+	}
 
 	@Override
 	public void render(float delta)
 	{
 		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
+		this.update(delta);
+
+		this.space.render(this.batch, this.cam);
+	}
+
+	public void update(float delta)
+	{
+		this.space.update(delta);
 	}
 
 	@Override
@@ -51,7 +80,7 @@ public class JastroScreen implements Screen
 	public void dispose()
 	{
 		// TODO Auto-generated method stub
-
+		this.space.dispose();
 	}
 
 }
